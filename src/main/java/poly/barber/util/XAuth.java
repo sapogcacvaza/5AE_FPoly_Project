3b02701d30b1f7cf28883f11db4f935139f8fc29
sapogcacvaza <vuthanhlong909@gmail.com>
2026-03-13 22:16:50 +0700

    init project

---
+++ src/main/java/poly/barber/util/XAuth.java
@@ -0,0 +1,14 @@
+//package poly.barber.util;
+//
+//import poly.cafe.entity.User;
+//
+//public class XAuth {
+//    public static User user = User.builder()
+//            .username("admin")
+//            .password("123")
+//            .enabled(true)
+//            .manager(true)
+//            .fullname("Lê Văn Nam")
+//            .photo("man.png")
+//            .build();
+//}
